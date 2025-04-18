77b4ba10f985841300cd513c5252048d63255646
cbacilio <dbacilio88@outlook.es>
2025-04-18 00:32:09 -0500

    feat(landing-component) init-project

---
+++ tutorial-java/project/api-bs-landing-components-microservice/src/test/java/com/bacsystem/landing/components/microservice/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bacsystem.landing.components.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+
+}
